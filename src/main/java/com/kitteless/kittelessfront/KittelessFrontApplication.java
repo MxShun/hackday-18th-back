80b58ac31d020d3e5c3a11204eca78598c583894
nozomi-ito <nozomi.ito2@askul.com>
2021-07-30 20:31:25 +0900

    DBに繋がる様になりました

---
+++ src/main/java/com/kitteless/kittelessfront/KittelessFrontApplication.java
@@ -0,0 +1,13 @@
+package com.kitteless.kittelessfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KittelessFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KittelessFrontApplication.class, args);
+    }
+
+}
