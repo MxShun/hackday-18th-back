28fe39be072d029c3e568473aed4890e73f00749
月岡 陽人 <akito-tsukioka@users.noreply.github.com>
2021-07-31 14:15:41 +0900

    コントローラ作った

--- src/main/java/com/kitteless/kittelessback/controller/PaymentController.java
+++ src/main/java/com/kitteless/kittelessback/controller/PaymentController.java
@@ -1,7 +1,13 @@
 package com.kitteless.kittelessback.controller;
 
+import com.kitteless.kittelessback.model.PaymentResponse;
+import com.kitteless.kittelessback.service.PaymentService;
 import com.kitteless.kittelessback.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,5 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PaymentController {
     @Autowired
-    UserService userService;
-}
+    PaymentService paymentService;
+
+    @PostMapping("/payment")
+    @ResponseStatus(HttpStatus.CREATED)
+    public PaymentResponse payment(@RequestBody Payment payment) {
+        return paymentService.payment(payment);
+    }
