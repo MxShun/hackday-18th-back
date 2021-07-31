13cf295c5184ee391f9a6232c1aae3ea16144c50
MxShun <16466322+MxShun@users.noreply.github.com>
2021-07-31 09:40:08 +0900

    RegisterControllerを作った”

---
+++ src/main/java/com/kitteless/kittelessback/controller/RegisterController.java
@@ -0,0 +1,24 @@
+package com.kitteless.kittelessback.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * アカウント登録API
+ */
+@Controller
+public class RegisterController {
+    @Autowired
+    RegisterService registerService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    // TODO 戻り値後で考える
+    public String register(@RequestBody User user) {
+        return registerService.resgister(user);
+    }
+}
