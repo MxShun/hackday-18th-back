208e373dc38ed67acaf7003e822460b839e841f3
MxShun <16466322+MxShun@users.noreply.github.com>
2021-07-31 14:49:59 +0900

    OCR APIのサービスまで作った

---
+++ src/main/java/com/kitteless/kittelessback/controller/OcrController.java
@@ -0,0 +1,24 @@
+package com.kitteless.kittelessback.controller;
+
+import com.kitteless.kittelessback.model.OcrResponse;
+import com.kitteless.kittelessback.service.OcrService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * OCR API
+ */
+@RestController
+public class OcrController {
+    @Autowired
+    OcrService ocrService;
+
+    @PostMapping("/extract")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public OcrResponse extract(String base64EncodedImage) {
+        return ocrService.extract();
+    }
+}
---
+++ src/main/java/com/kitteless/kittelessback/service/OcrService.java
@@ -0,0 +1,20 @@
+package com.kitteless.kittelessback.service;
+
+import com.kitteless.kittelessback.model.OcrResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OcrService {
+    @Autowired
+    OcrRepository ocrRepository;
+
+    public OcrResponse extract(String base64EncodedImage) {
+        String result = ocrRepository.read(base64EncodedImage);
+
+        OcrResponse ocrResponse = new OcrResponse();
+        ocrResponse.setResult("success");
+        ocrResponse.setValue(result);
+        return ocrResponse;
+    }
+}
