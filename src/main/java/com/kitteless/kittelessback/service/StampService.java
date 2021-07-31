f21790c508cce9f819ef09900040d8e0df5f63be
Yuya Kotani <yuya.kotani@askul.com>
2021-07-31 18:36:11 +0900

    /stamp/verify の骨子を作った

--- src/main/java/com/kitteless/kittelessback/service/StampService.java
+++ src/main/java/com/kitteless/kittelessback/service/StampService.java
@@ -1,6 +1,7 @@
 package com.kitteless.kittelessback.service;
 
 import com.kitteless.kittelessback.model.StampAuthorizeResponse;
+import com.kitteless.kittelessback.model.StampVerifyResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class StampService {
@@ -14,5 +15,18 @@ public class StampService {
         StampAuthorizeResponse stampAuthorizeResponse = new StampAuthorizeResponse();
         stampAuthorizeResponse.setResult("success");
         return stampAuthorizeResponse;
+    }
+
+    public StampVerifyResponse stampVerify(String image) {
+
+        // TODO:OCRで画像をもとに9桁の切手コードを取得する
+
+        // TODO:その切手コードをもとに決済テーブルから対象の決済を取得する
+
+        // TODO:取得結果に応じてreturnする
+
+        StampVerifyResponse stampVerifyResponse = new StampVerifyResponse();
+        stampVerifyResponse.setResult("success");
+        return stampVerifyResponse;
     }
 }
