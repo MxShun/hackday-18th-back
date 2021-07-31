32c50320f8b2cc0b825ee02304dadf4885195534
MxShun <16466322+MxShun@users.noreply.github.com>
2021-07-31 12:57:24 +0900

    insertをignoreをする

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
-INSERT INTO kitteless.user(id, name, password) VALUES ("12345", "hoge", "fuga");
+INSERT IGNORE INTO kitteless.user(id, name, password) VALUES
+    ("12345", "hoge", "fuga"),
+    ("54321", "piyo", "piko");
