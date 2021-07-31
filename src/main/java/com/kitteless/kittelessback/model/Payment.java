7c7dc9a34b1eef2afb1a32e42d81f1631bb1de36
月岡 陽人 <akito-tsukioka@users.noreply.github.com>
2021-07-31 15:15:52 +0900

    DBクラス作った

---
+++ src/main/java/com/kitteless/kittelessback/model/Payment.java
@@ -0,0 +1,20 @@
+package com.kitteless.kittelessback.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "payment")
+@Data
+public class Payment {
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "userId")
+    private String userId;
+
+    @Column(name = "amount")
+    private int amount;
+}
