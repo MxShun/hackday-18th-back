5fa163b8401d86f220f7a2d90369cb21d04b0e61
MxShun <16466322+MxShun@users.noreply.github.com>
2021-08-01 18:24:14 +0900

    Hisotryのリポジトリ部分を作った

--- src/main/java/com/kitteless/kittelessback/controller/PaymentController.java
+++ src/main/java/com/kitteless/kittelessback/controller/PaymentController.java
@@ -3,7 +3,6 @@ package com.kitteless.kittelessback.controller;
 import com.kitteless.kittelessback.model.Payment;
 import com.kitteless.kittelessback.model.PaymentResponse;
 import com.kitteless.kittelessback.service.PaymentService;
-import com.kitteless.kittelessback.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/kitteless/kittelessback/service/HistoryService.java
+++ src/main/java/com/kitteless/kittelessback/service/HistoryService.java
@@ -1,25 +1,29 @@
 package com.kitteless.kittelessback.service;
 
 import com.kitteless.kittelessback.model.HistoryResponse;
+import com.kitteless.kittelessback.model.Payment;
+import com.kitteless.kittelessback.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class HistoryService {
-//    TODO Repository実装
-//    @Autowired
-//    PaymentRepository paymentRepository;
+    @Autowired
+    PaymentRepository paymentRepository;
 
     public List<HistoryResponse> getHistory(String userId) {
-        // TODO Repository実装
+        List<Payment> paymentList = paymentRepository.findPaymentsByUserId(userId);
 
-        HistoryResponse mockHitoryResponse = new HistoryResponse();
-        mockHitoryResponse.setDateTime(LocalDateTime.now());
-        mockHitoryResponse.setAmount(84);
-        return List.of(
-            mockHitoryResponse
-        );
+        return paymentList.stream()
+                .map(payment -> {
+                    HistoryResponse historyResponse = new HistoryResponse();
+                    historyResponse.setDateTime(payment.getDateTime());
+                    historyResponse.setAmount(payment.getAmount());
+                    return historyResponse;
+                })
+                .collect(Collectors.toList());
     }
 }
