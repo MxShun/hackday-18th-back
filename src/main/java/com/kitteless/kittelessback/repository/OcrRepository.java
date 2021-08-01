4c364cdc481d4a22411d5cfd5d6ba35d724e4aff
MxShun <16466322+MxShun@users.noreply.github.com>
2021-08-01 10:51:28 +0900

    不要なコメント削除

--- src/main/java/com/kitteless/kittelessback/repository/OcrRepository.java
+++ src/main/java/com/kitteless/kittelessback/repository/OcrRepository.java
@@ -30,13 +30,12 @@ public class OcrRepository {
     }
 
     private String doPost(String base64) {
-        String apiURL = "https://bef2c44061654479868834a873039292.apigw.ntruss.com/custom/v1/464/2580fcb43bc90fe421f10c7206a0f8ae6fd43667eaeda7c4d679e21910b0b1e9/general";
-        String secretKey = "Q05vcnJzTUd0eE1mYVFVWkpDVHhNclFzcWR5T1p3VnM=";
+        final String CLOVA_OCR_CUSTOM_API = "https://bef2c44061654479868834a873039292.apigw.ntruss.com/custom/v1/464/2580fcb43bc90fe421f10c7206a0f8ae6fd43667eaeda7c4d679e21910b0b1e9/general";
+        final String CLOVA_OCR_CUSTOM_SECRET = "Q05vcnJzTUd0eE1mYVFVWkpDVHhNclFzcWR5T1p3VnM=";
 
         StringBuffer response = new StringBuffer();
         try {
-            URL url = new URL(apiURL);
-//            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            URL url = new URL(CLOVA_OCR_CUSTOM_API);
             HttpsURLConnection con = (HttpsURLConnection)url.openConnection();
             con.setUseCaches(false);
             con.setDoInput(true);
@@ -45,7 +44,7 @@ public class OcrRepository {
             con.setRequestMethod("POST");
             String boundary = "----" + UUID.randomUUID().toString().replaceAll("-", "");
             con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-            con.setRequestProperty("X-OCR-SECRET", secretKey);
+            con.setRequestProperty("X-OCR-SECRET", CLOVA_OCR_CUSTOM_SECRET);
 
             JSONObject json = new JSONObject();
             json.put("version", "V2");
@@ -61,7 +60,6 @@ public class OcrRepository {
 
             con.connect();
             DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-            long start = System.currentTimeMillis();
             writeMultiPart(wr, postParams, base64, boundary);
             wr.close();
 
@@ -77,11 +75,8 @@ public class OcrRepository {
                 response.append(inputLine);
             }
             br.close();
-
-            System.out.println(postParams);
-            System.out.println(response);
         } catch (Exception e) {
-            System.out.println(e);
+            // 握りつぶす
         }
 
         return response.toString();
@@ -110,14 +105,6 @@ public class OcrRepository {
 
 
             byte[] bytes = Base64.getDecoder().decode(base64);
-//            try (FileInputStream fis = new FileInputStream(file)) {
-//                byte[] buffer = new byte[8192];
-//                int count;
-//                while ((count = fis.read(buffer)) != -1) {
-//                    out.write(buffer, 0, count);
-//                }
-//                out.write("\r\n".getBytes());
-//            }
             out.write(bytes);
             out.write("\r\n".getBytes());
 
