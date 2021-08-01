6a7f7e5d571c171187ca9cee5477ba072c8dd2ec
MxShun <16466322+MxShun@users.noreply.github.com>
2021-08-01 14:50:57 +0900

    Postmappingだった(てへ)

--- src/main/java/com/kitteless/kittelessback/controller/ChargeController.java
+++ src/main/java/com/kitteless/kittelessback/controller/ChargeController.java
@@ -4,7 +4,7 @@ import com.kitteless.kittelessback.model.ChargeResponse;
 import com.kitteless.kittelessback.service.ChargeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
